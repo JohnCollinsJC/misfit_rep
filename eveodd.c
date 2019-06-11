0e6bab3132531b50246c4bc7fbca001c68825a9a
JohnCollinsJC <51689312+JohnCollinsJC@users.noreply.github.com>
2019-06-11 10:49:09 +0530

    Update eveodd.c

--- eveodd.c
+++ eveodd.c
@@ -1,21 +1,22 @@
 #include<stdio.h>
-#include<stdlib.h>
+#include<ctype.h>
 int main(){
 	int n;
-	char *end;
-	char str[100];
-	scanf("%s",str);
-	n= strtol(str, &end, 10);
-	if(*end==0){
+// 	char *end;
+// 	char str[100];
+	scanf("%d",&n);
+// 	n= strtol(str, &end, 10);
+// 	if(isdigit(n)){
+	   // printf(".");
 	    if(n<0)
 	    printf("invalid");
 	    else if(!(n%2))
 	    printf("Even");
 	    else
 	    printf("Odd");
-	}
-	else
-	printf("invalid");
-//  	printf(" %d %d",n,*end);
+// 	}
+// 	else
+// 	printf("invalid");
+//  	printf(" %d",n);
 	return 0;
 }
