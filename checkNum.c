2746d08e3c39b4ab0d80e2e2b41dd84d583b8b80
JohnCollinsJC <51689312+JohnCollinsJC@users.noreply.github.com>
2019-06-11 09:25:53 +0530

    Update checkNum.c

--- checkNum.c
+++ checkNum.c
@@ -1,12 +1,17 @@
 #include<stdio.h>
+#include<ctype.h>
 int main(){
 	int num;
 	scanf("%d",&num);
+	if(isdigit(num)){
 	if(num==0)
 	printf("Zero");
 	else if(num>0)
 	printf("Positive");
 	else
 	printf("Negative");
+	}
+	else
+		printf("Invalid Input");
 	return 0;
 }
