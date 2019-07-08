7f538592decf91d6f6f9cce9064b23e4d30393c1
Aokia John Collins M <51689312+JohnCollinsJC@users.noreply.github.com>
2019-07-08 19:36:00 +0530

    Create addstring.c

---
+++ addstring.c
@@ -0,0 +1,20 @@
+
+#include <string.h>
+#include <stdio.h>
+
+int main()
+{
+    char str1[100], str2[100];
+    scanf("%s %s",str1,str2);
+    
+    int len=strlen(str1);
+    char str3[len];
+    
+    for(int i=0; i<len; i++){
+        str3[i]=(str1[i]-'a'+str2[i]-'a')%26+'b';
+    }
+    
+    printf("%s",str3);
+    
+    return 0;
+}
